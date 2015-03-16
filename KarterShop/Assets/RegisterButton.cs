5db167a6dc0596a38c8c64ff33b10b1880bce99e
jmontineri <jmontineri@gmail.com>
2015-03-16 21:30:17 +0100

    Registration functionality

---
+++ KarterShop/Assets/RegisterButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RegisterButton : MonoBehaviour {
+
+	public Text _username, _password, _confirmPassword, _nnid;
+
+	public void Register(){
+		string user, pass, nnid;
+
+		user = _username.text;
+		pass = _password.text;
+		nnid = _nnid.text;
+
+		if (pass != _confirmPassword.text) {
+			return;
+			//We'll need to tell the user that their passes dont match
+		}
+
+		WWWForm form = new WWWForm ();
+
+		form.AddField ("username", user);
+		form.AddField ("password", pass);
+		form.AddField ("nnid", nnid);
+
+		WWW request = new WWW ("http://winepicgaming.de/mks/register.aspx", form);
+
+		
+
+		while (!request.isDone)
+			;
+		Debug.Log (request.text);
+
+
+	}
+}
